cabbcbb624acd0b6a61cb3d8e2d9da4f7847892a
Nox <rafaelquintanilha@hotmail.com>
2022-04-12 22:13:19 -0300

    esqueci de comentar uma coisa =x

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -4,20 +4,20 @@
 
 // (Questão 1)
 
-//let nome
-//let idade
-//console.log(typeof nome, typeof idade)   //A falta de declaração me retornou um undefined para ambos.
+let nome
+let idade
+console.log(typeof nome, typeof idade)   //A falta de declaração me retornou um undefined para ambos.
+
+/////////////////
+
+nome = prompt("Qual seu nome ?")
+idade = prompt("Qual sua idade ?")
+console.log(typeof nome, typeof idade)   //Os numeros dados informados pelo prompt deram ao typeof a possibilidade de identificar que nome é do tipo string e idade tipo number.
 
 /////////////////
 
 //let nome = prompt("Qual seu nome ?")
 //let idade = prompt("Qual sua idade ?")
-//console.log(typeof nome, typeof idade)   //Os numeros dados informados pelo prompt deram ao typeof a possibilidade de identificar que nome é do tipo string e idade tipo number.
-
-/////////////////
-
-let nome = prompt("Qual seu nome ?")
-let idade = prompt("Qual sua idade ?")
 console.log("Olá", nome, ", você tem", idade, "anos")
 
 // (Questão 2)
@@ -33,8 +33,7 @@ console.log("Você ama TI ? - ", terceiraPergunta)
 
 let a = 10
 let b = 25
-let c
-c = a
+let c = a
 a = b
 b = c
 console.log("O novo valor de a é:", a) 
