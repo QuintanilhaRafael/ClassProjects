025aec234eb0734116289e1424c704b55603f460
Nox <rafaelquintanilha@hotmail.com>
2022-05-13 18:41:21 -0300

    revisao-js completa

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,19 +5,19 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  //return array.reverse()
-  
- let tamanho = array.length
- let array2 = []
- for (i=0; i<tamanho; i++){
-array2[i] = array.pop()
- } 
- return array2
+    //return array.reverse()
+
+    let tamanho = array.length
+    let array2 = []
+    for (i = 0; i < tamanho; i++) {
+        array2[i] = array.pop()
+    }
+    return array2
 
 
 }
@@ -25,19 +25,19 @@ array2[i] = array.pop()
 function retornaArrayOrdenado(array) {
     //return array.sort(function(a, b){return a-b})
 
-    var teste = false 
-    while (!teste){
+    var teste = false
+    while (!teste) {
         teste = true
-  for (i = 1; i < array.length; i+=1){
-      if (array[i-1] > array[i]){
-          teste = false
-          var pocket = array[i-1]
-          array[i-1] = array[i]
-          array[i] = pocket
-      }
-  }
-}
-return array
+        for (i = 1; i < array.length; i += 1) {
+            if (array[i - 1] > array[i]) {
+                teste = false
+                var pocket = array[i - 1]
+                array[i - 1] = array[i]
+                array[i] = pocket
+            }
+        }
+    }
+    return array
 }
 
 
@@ -51,23 +51,23 @@ function retornaNumerosPares(array) {
         }
     })
     return newArray*/
-let newArray = []
-    for (i=0;i<array.length;i++){
-        if (array[i]%2===0){
-newArray.push(array[i])
+    let newArray = []
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            newArray.push(array[i])
         }
     }
     return newArray
-  
+
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- let newArray = array.filter((number)=>{
-        if (number%2===0){
+    let newArray = array.filter((number) => {
+        if (number % 2 === 0) {
             return number
         }
-    }).map((number)=>{
+    }).map((number) => {
         return Math.pow(number, 2)
     })
     return newArray
@@ -75,22 +75,24 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-    
-  
+    var maiorNum = array.reduce((a, b) => {
+        return Math.max(a, b)
+    })
+    return maiorNum
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-    if (num1 >= num2){
+    if (num1 >= num2) {
         return {
             maiorNumero: num1,
-            maiorDivisivelPorMenor: num1%num2 == 0,
+            maiorDivisivelPorMenor: num1 % num2 == 0,
             diferenca: num1 - num2
         }
     } else {
         return {
             maiorNumero: num2,
-            maiorDivisivelPorMenor: num2%num1 == 0,
+            maiorDivisivelPorMenor: num2 % num1 == 0,
             diferenca: num2 - num1
         }
     }
@@ -98,78 +100,98 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+    let array = [...Array(n * 2).keys()]
+    newArray = []
+    for (i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray.push(i)
+        }
+    }
+    return newArray
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if (ladoA === ladoB && ladoB === ladoC) {
+        return `Equilátero`
+    } else if (ladoA !== ladoB && ladoB !== ladoC && ladoC !== ladoA) {
+        return `Escaleno`
+    } else {
+        return `Isósceles`
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    array.sort((a, b) => { return a - b })
+    let novoArray = [array[array.length - 2], array[1]]
+    return novoArray
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores.join(', ')}.`
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    return {
+        ...pessoa, nome: 'ANÔNIMO'
+    }
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+    const listaPermitida = []
+    for (let pessoa of pessoas) {
+        if (pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura >= 1.5) {
+            listaPermitida.push(pessoa)
+        }
+    }
+    return listaPermitida
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+    const listaNegada = []
+    for (let pessoa of pessoas) {
+        if (pessoa.idade <= 14 || pessoa.idade > 60 || pessoa.altura < 1.5) {
+            listaNegada.push(pessoa)
+        }
+    }
+    return listaNegada
 }
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
     let divida = 0
-for (let conta of contas){
-    for(i=0;i<conta.compras.length; i++){
-         divida += conta.compras[i]
-    }
-    conta.compras = []
-    conta.saldoTotal -= divida
-    divida = 0
-
-}
-return contas
+    contas.forEach(conta => {
+        for (i = 0; i < conta.compras.length; i++) {
+            divida += conta.compras[i]
+        }
+        conta.compras = []
+        conta.saldoTotal -= divida
+        divida = 0
+    })
+    return contas
 }
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+    let novaOrdemNomes = consultas.sort((a, b) => { return a.nome.toLowerCase() > b.nome.toLowerCase() ? 1 : a.nome.toLowerCase() < b.nome.toLowerCase() ? -1 : 0 })
+    return novaOrdemNomes
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-        const newConsultas = consultas.map((item)=>{
-            return item.dataDaConsulta
-        }).sort(function (a, b) {
-            a = a.split('/');
-            b = b.split('/');
-            return a[2] - b[2] || a[1] - b[1] || a[0] - b[0];                                                                           
-        });
-        var novaOrdem = []
-        for(i=0;i<consultas.length;i++){
-            for(j=0;j<consultas.length;j++){
-            if (newConsultas[i]===consultas[j].dataDaConsulta){
-                novaOrdem.push(consultas[j])
-            }
-        }
-    }
-    return novaOrdem
-    
+    const newConsultas = consultas.sort((a, b) => {
+        a = a.dataDaConsulta.split('/')
+        b = b.dataDaConsulta.split('/')
+        return a[2] - b[2] || a[1] - b[1] || a[0] - b[0]
+    })
+    return newConsultas
+
 }
 
 
