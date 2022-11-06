c1d202825772e692cf4672fccebc7996fa515112
Rafael <rafaelquintanilha@hotmail.com>
2022-11-06 18:38:45 -0300

    problem

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,1 +1,31 @@
-console.log('gess')
+import express, {Request, Response} from "express"
+
+import { toDoList } from "./data"
+
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req:Request, res:Response) => {          
+    res.send("Pong")
+})
+
+app.get('/todos', (req:Request, res:Response) => {      
+    const toDosList = toDoList.map((item)=> {
+        return item
+    })   
+    res.send(toDosList)
+})
+
+app.get('/todos:id', (req:Request, res:Response) => {      
+    const status = Number(req.params.id)
+    console.log(status)
+})
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003");
+});
